8144fcce0cc8cbd680e71b87374264b18bdf95e8
Pedro <pedrocolomaa@gmaail.com>
2020-05-20 15:37:41 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Pedro Felipe Coloma de Araujo on 20/05/20.
+//  Copyright © 2020 Pedro Felipe Coloma de Araujo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
